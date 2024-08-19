f67df939e448dc2557d27a67987f07b97e7368ae
Ksenia Kobak <kseniakobka@yandex.ru>
2024-08-19 17:01:34 +0300

    Initial Commit

---
+++ WheatherApp_TestFarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WheatherApp_TestFarm
+//
+//  Created by Ксения Кобак on 19.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
